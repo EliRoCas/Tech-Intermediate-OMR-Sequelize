094828c08da9e89967f11b39540ba6b5b4b058e6
Eli Rosero <elir_94@hotmail.com>
2024-04-22 21:38:58 -0500

    delete and put

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = require("../router/product.router");
-const morgan = require ("morgan")
+const morgan = require("morgan")
 
 const app = express();
 
@@ -10,7 +10,11 @@ app.get('/', (req, res) => {
     res.send("Esta es una API con ORM Sequelize");
 });
 
+// Se le indica que pueda leer json - Debe ir antes de la ruta a trabajar 
+app.use(express.json());
+
 // Se le indica en qué ruta se va a trabajar 
 app.use("/api/v1", router);
+
 
 module.exports = app; 
--- src/router/product.router.js
+++ src/router/product.router.js
@@ -30,13 +30,29 @@ router.get("/products/:product_id", async (req, res) => {
 })
 
 
-// Método para agregar productos 
+// Método para agregar productos utilizando faker 
+// router.post("/products", async (req, res) => {
+//         await Products.sync();
+//     const createProduct = await Products.create({
+//         product_name: faker.commerce.product(),
+//         price: faker.commerce.price(),
+//         is_stock: faker.datatype.boolean()
+//     })
+//     res.status(201).json({
+//         ok: true,
+//         status: 201,
+//         message: "Producto Creado con éxito."
+//     })
+// })
+
+// Método para agregar productos utilizando JSON  
 router.post("/products", async (req, res) => {
+    const dataProducts = req.body
     await Products.sync();
     const createProduct = await Products.create({
-        product_name: faker.commerce.product(),
-        price: faker.commerce.price(),
-        is_stock: faker.datatype.boolean()
+        product_name: dataProducts.product_name,
+        price: dataProducts.price,
+        is_stock: dataProducts.is_stock
     })
     res.status(201).json({
         ok: true,
@@ -45,14 +61,51 @@ router.post("/products", async (req, res) => {
     })
 })
 
+
 // Método para editar los productos 
-router.put("/products", (req, res) => {
-    res.send("Soy un Touter");
+// Put nos da una actualización completa del elemento, todo lo que sea manejable por el usuario
+router.put("/products/:product_id", async (req, res) => {
+    const id = req.params.product_id;
+    const dataProducts = req.body;
+    const updateProduct = await Products.update(
+        {
+            product_name: dataProducts.product_name,
+            price: dataProducts.price,
+            is_stock: dataProducts.is_stock,
+        }, {
+        where: {
+            product_id: id,
+        }
+    }
+    );
+    res.status(200).json({
+        ok: true,
+        status: 200,
+        body: updateProduct
+    })
+
 })
 
+
 // Método para eliminar un producto 
-router.delete("/products", (req, res) => {
-    res.send("Soy un Touter");
+router.delete("/products/:product_id", async (req, res) => {
+    const id = req.params.product_id;
+    const deleteProduct = await Products.destroy({
+        where: {
+            product_id: id,
+        }
+    });
+    // Con código de error que no envía el json
+    // res.status(204).json({
+    //     ok: true,
+    //     status: 204,
+    //     body: deleteProduct
+    // })
+    res.status(200).json({
+        ok: true,
+        status: 204,
+        body: deleteProduct
+    })
 })
 
 
